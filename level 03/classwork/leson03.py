0ea29666faa67e2272d54aaaf7992d80b54f6dbc
GiorgiTsukhishvili07 <giorgitsukhishvili07@gmail.com>
2024-05-25 04:29:02 -0700

    commit

---
+++ level 03/classwork/leson03.py
@@ -0,0 +1,9 @@
+#ცვლადი არის რაღაცის სახელი. წარმოვიდგინოთ რაღაც ყუთი მაგრამ  ყუთი არ იცვლება. იცვლება რაც ყუთში ფვაქვს
+
+
+
+company = "apple"
+month = "march"
+book = "robizon cruzo"
+country = "george"
+point = '364'
