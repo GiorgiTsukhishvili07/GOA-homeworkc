4092dee5eedae6411d71d47660e43efc0b4df9a6
GiorgiTsukhishvili07 <giorgitsukhishvili07@gmail.com>
2024-07-01 04:35:59 -0700

    commit

---
+++ level 06/homework.py
@@ -0,0 +1,12 @@
+name = "data"             # შექმნილმა ცვლადმა name (სახელი) მინშვნელობა მოგვანიჭა"data"
+surname = "diasamize"     # შექმნილმა ცვლადმა surname (გვარი) მინშვნელობა მოგვანიჭა "diasamize"
+ege = "100"               # შექმნილმა ცვლადმა age (ასაკი)  მინშვნელობა მოგვანიჭა "100"
+text = "I am programmer"  # შექმნილმა ცვლადმა text (ტექსტი) მინშვნელობა მოგვანიჭა "I am programmer"
+
+
+
+                                    #davaleba 2
+name = "giorgi"
+surname = "tshukhishvili"
+age = "13"
+text = "I am programmer"
